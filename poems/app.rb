5e0b0249b9a55ec78c6d39b5e208e3816fe92976
Greg Borenstein <greg.borenstein@gmail.com>
2013-11-14 10:57:43 -0500

    add debug option to API, integrate it into map page

--- poems/app.rb
+++ poems/app.rb
@@ -1,7 +1,7 @@
 require 'sinatra'
 require 'json'
 require './status.rb'
-
+use Rack::Logger
 
 # Cambridge: 42.36024,-71.08745
 # Portland: 45.5200,122.6819
@@ -9,9 +9,21 @@ require './status.rb'
 FORECAST_KEY = "23b22b200d5ff8700237a133c6d39b5e"
 
 get "/:lat,:lng" do
+	request.logger.info params.inspect
+
 	status = Status.new({:lat => params[:lat], 
 						 :lng => params[:lng], 
 						 :forecast_key => FORECAST_KEY})
 
-	status.message
+	if params["debug"]
+		{"message" => status.message, 
+		 "location" => {"lat" => params[:lat], "lng" => params[:lng]},
+		 "forecast" => {"temperature" => status.forecast.currently.apparentTemperature, 
+		 				"time" => Time.at(status.forecast.currently.time),
+		 				"clouds" => status.forecast.currently.cloudCover,
+		 				"precipType" => status.forecast.daily.data[0]["precipType"],
+		 				"precipIntensity" =>  status.forecast.daily.data[0]["precipIntensity"]}}.to_json
+	else 
+		status.message
+	end
 end
